a70b29df963d24fbf2cd1cfbd397a965dcc6a972
OneWinterSigh <cabelloruizgerman@gmail.com>
2023-04-21 12:33:28 +0200

    Añadidas tablas varias

--- practica2.sql
+++ practica2.sql
@@ -5,7 +5,27 @@ CREATE TABLE Local (
   'codigo' int NOT NULL,
   'tiene_cafeteria' boolean NOT NULL,
   'codigo_postal' int NOT NULL,
-  'calle' char(30) NOT NULL,
+  'calle' varchar(30) NOT NULL,
   'numero' int NOT NULL,
-  PRIMARY KEY ('codigo');
+  PRIMARY KEY ('codigo')
+);
+
+CREATE TABLE Poblacion (
+  'codigo' int NOT NULL,
+  'nombre' varchar(20) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Provincia (
+  'codigo' int NOT NULL,
+  'nombre' varchar(30) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Producto (
+  'codigo' int NOT NULL,
+  'nombre' varchar(30) NOT NULL,
+  'descripcion' varchar(100) NOT NULL,
+  'precio_base' int NOT NULL,
+  PRIMARY KEY('codigo')
 );
