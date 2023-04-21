a604c0475ecce7db16966776c03c09bca74dd2f6
OneWinterSigh <cabelloruizgerman@gmail.com>
2023-04-21 15:17:35 +0200

    Añadidas tablas para las entidades

--- practica2.sql
+++ practica2.sql
@@ -29,3 +29,42 @@ CREATE TABLE Producto (
   'precio_base' int NOT NULL,
   PRIMARY KEY('codigo')
 );
+
+CREATE TABLE Categoria (
+  'codigo' int NOT NULL,
+  'descripcion' varchar(100) NOT NULL,
+  'tipo_producto' varchar(20) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Modo_Cocinado (
+  'codigo' int NOT NULL,
+  'descripcion' varchar(100) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Ingrediente (
+  'codigo' int NOT NULL,
+  'descripcion' varchar(100) NOT NULL,
+  'nombre' varchar(30) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Empleado (
+  'codigo' int NOT NULL,
+  'telefono_contact' int NOT NULL,
+  'nss' int NOT NULL,
+  'dni' int NOT NULL,
+  'nombre' varchar(20) NOT NULL,
+  'primer_apellido' varchar(20) NOT NULL,
+  'segundo_apellido' varchar(20) NOT NULL,
+  PRIMARY KEY('codigo')
+);
+
+CREATE TABLE Tarea (
+  'codigo' int NOT NULL,
+  'descripcion' varchar(100) NOT NULL,
+  'coste' int NOT NULL,
+  'prerrequisitos' varchar(1000) NOT NULL,
+  PRIMARY KEY('codigo')
+);
